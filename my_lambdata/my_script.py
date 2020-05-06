03de9dd506ea29823b00fdbade6c1ee59d958c72
Joseph-Maulin <joseph.l.maulin@gmail.com>
2020-05-05 23:56:16 -0400

    added __init__

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,8 +1,11 @@
 
 import pandas as pd
+from my_lambdata.my_mod import enlarge
 
 print("HELLO")
 
 df = pd.DataFrame({"a":[1,2,3], "b":[4,5,6]})
 
 print(df.head())
+
+print(enlarge(df["a"].sum()))
